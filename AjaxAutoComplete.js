d196b3da42e3df9db5253a09fab36f14fe12c2e1
Luís Barbosa <luisrgbarbosa@gmail.com>
2013-04-03 11:08:57 +0200

    Update AjaxAutoComplete.js

--- AjaxAutoComplete.js
+++ AjaxAutoComplete.js
@@ -3,16 +3,6 @@
     //un = undefined to avoid undefined redifinition
     function Autocomplete(context, options) {
         var self = this,
-            timeOut = null,
-            searchElement = null,
-            resultsElement = null,
-            currentOptions = {},
-            execBlur = true,
-            xhrRequest = null,
-            currentSelect = -1,
-            resultsElementChildren = 0,
-            isLocal = false,
-            responseCache = [],
             defaultOptions = {
                 source : '',
                 limit : 10,
@@ -31,183 +21,19 @@
                 onFocusCallback : function (element) {},
                 onBlurCallback : function (element) {}
             };
-               
-         //Public methods
-         self.clearAutocomplete = function () {
-            clearTimeout(timeOut);
-            currentSelect = -1;
-            resultsElement.hide();
-         }
 
-         //Private methods
-         function autocomplete(value) {
-            if (value.length < currentOptions.minChars) {
-                self.clearAutocomplete();
-                return;
-            }
-            
-            var localData = isLocal ? getLocalData(value) : responseCache[value];
-   
-            if (localData) {
-                fillSuggestionsList(value, localData);
-                return;
-            }
-
-            if (xhrRequest) {
-                xhrRequest.abort();  
-            }
-            
-            xhrRequest = $.get(currentOptions.source, { term : value, limit: currentOptions.limit }, function (data) {
-                processResponse(value, data);
-                currentOptions.requestCallback(data);
-            }, (currentOptions.returnJSON) ? 'json' : '');
-         }
-        
-         function selectOption() {
-            execBlur = true;
-            var selected = resultsElement.find('.current'), 
-                selectedData = selected 
-                               ? $.data(selected[0], 'autocomplete') 
-                               : {};
-            searchElement.val(currentOptions.formatResult(selectedData)).blur();
-            currentOptions.onSelectionCallback(selectedData);
-            self.clearAutocomplete();
-         }
-        
-         function optionNavigation(upArrow) {
-            upArrow = upArrow || false;
-            execBlur = false;
-          
-            if (upArrow) {
-                currentSelect = (currentSelect == 0) ? resultsElementChildren - 1 : --currentSelect;
-            } else {
-                currentSelect = (++currentSelect % resultsElementChildren)
-            }
-            
-            resultsElement.find('.current')
-                .removeClass('current')
-                .end()
-                .find('li:eq(' + currentSelect + ')')
-                .addClass('current')
-                .end();
-         }
-
-         function limitNumberOfItems(dataLength) {
-		    return currentOptions.limit && currentOptions.limit < dataLength
-			    ? currentOptions.limit
-			    : dataLength;
-	     }
-
-         function getLocalData(term) {
-            var predicate = currentOptions.lookupPredicate;
-            
-            return $.grep(currentOptions.source, function (row) {
-                return predicate(row, term);
-            });
-         }
-
-         function processResponse(term, data) {
-            // Cache results if cache is not disabled:
-            if (!currentOptions.noCache) {
-                responseCache[term] = data;
-            }
-
-            fillSuggestionsList(term, data);   
-         }
-
-         function fillSuggestionsList(term, data) {
-            if (data.length == 0) {
-                return;
-            }
-
-            var totalItems = limitNumberOfItems(data.length),
-                docFragment = document.createDocumentFragment(),
-                listItem, resultItem, formatedItem;
-
-            resultsElement.empty();
-            for (var i = 0; i < totalItems; ++i) {
-                formatedItem = currentOptions.formatItem(data[i]);
-                listItem = document.createElement('li');
-                listItem.innerHTML = Autocomplete.highlightTerm(formatedItem, term);
-                docFragment.appendChild(listItem);
-                $.data(listItem, 'autocomplete', data[i]);
-            }
-            resultsElement.append(docFragment).show();
-            resultsElementChildren = $('li', resultsElement).length;
-            currentSelect = -1;
-         }
-        
-         function bindEvents() {
-            searchElement.keydown(function (ev) {
-                var keyCode = ev.keyCode || window.event.keyCode, 
-                    keyCodes = Autocomplete.keyCodes;
-              
-                switch (keyCode) {
-                    case keyCodes.ENTER: //Enter
-                        selectOption();
-                        break;
-                    case keyCodes.ESCAPE: //Escape
-                        self.clearAutocomplete(); 
-                        break;
-                    case keyCodes.UP: //Up and down arrows
-                    case keyCodes.DOWN:
-                        optionNavigation.call(self, keyCode == keyCodes.UP ? true : false);
-                        currentOptions.upDownArrowsCallback.call(self, keyCode);
-                        break;
-                }
-            }).keyup(function (ev) {
-                var that = $(this),
-                    keyCode = ev.keyCode || window.event.keyCode, 
-                    keyCodes = Autocomplete.keyCodes;
-                    
-                if ((keyCode >= keyCodes.LETTERS_NUMBERS_MIN && keyCode <= keyCodes.LETTERS_NUMBERS_MAX) 
-                    || (keyCode >= keyCodes.NUM_PAD_MIN && keyCode <= keyCodes.NUM_PAD_MAX) 
-                    || keyCode == keyCodes.SUBTRACT 
-                    || keyCode == keyCodes.FORWARD_SLASH || keyCode == keyCodes.BACK_SLASH 
-                    || keyCode == keyCodes.BACKSPACE 
-                    || keyCode == keyCodes.DELETE) { //Only numbers, letters, hyphen, back slash, forward slash, backspace, delete
-                    if (currentOptions.deferRequestBy > 0) {
-                        timeOut = setTimeout(function () { 
-                            autocomplete(that.val());
-                        }, currentOptions.deferRequestBy);
-                    } else {
-                        autocomplete(that.val());
-                    }
-                }   
-            }).focus(function () {
-                self.clearAutocomplete();
-                currentOptions.onFocusCallback.call(self, $(this));
-            }).blur(function () {
-                if (execBlur) {
-                    self.clearAutocomplete();
-                    currentOptions.onBlurCallback.call(self, $(this));
-                }
-            });
-            
-            resultsElement.mouseenter(function () {
-                execBlur = false;
-            }).mouseleave(function () {
-                execBlur = true;
-            }).mouseover(function (ev) {
-                if (ev.target.nodeName && ev.target.nodeName.toUpperCase() == 'LI') {
-                    resultsElement.find('.current').removeClass('current');
-                    $(ev.target).addClass('current');
-                }
-            }).click(function () {
-                selectOption();
-            });
-         }
-        
-         function init() {
-            currentOptions = $.extend({}, defaultOptions, options);
-            isLocal = $.isArray(currentOptions.source) ? true : false;
-            searchElement = $(context).attr('autocomplete', 'off');
-            resultsElement = $('<ul></ul>', { class : 'autocomplete-results' }).insertAfter(searchElement);
-            bindEvents();
-         }
-        
-         //Run initializer
-         init();
+         self.timeOut = null;
+         self.currentOptions = $.extend({}, defaultOptions, options);
+         self.isLocal = $.isArray(self.currentOptions.source) ? true : false;
+         self.searchElement = $(context).attr('autocomplete', 'off');
+         self.resultsElement = $('<ul></ul>', { class : 'autocomplete-results' }).insertAfter(self.searchElement);
+         self.responseCache = [];
+         self.execBlur = true;
+         self.xhrRequest = null;
+         self.currentSelect = -1;
+         self.resultsElementChildren = 0;
+         
+         self.bindEvents();
     };
 
     Autocomplete.highlightTerm = function (value, term) {
@@ -230,6 +56,178 @@
         BACK_SLASH : 220,
         BACKSPACE : 8,
         DELETE : 46
+    };
+
+    Autocomplete.prototype = {
+        clearAutocomplete : function () {
+            clearTimeout(this.timeOut);
+            this.currentSelect = -1;
+            this.resultsElement.hide();
+         },
+         
+         autocomplete : function (value) {
+            var self = this;
+
+            if (value.length < self.currentOptions.minChars) {
+                self.clearAutocomplete();
+                return;
+            }
+            
+            var localData = self.isLocal ? self.getLocalData(value) : self.responseCache[value];
+   
+            if (localData) {
+                self.fillSuggestionsList(value, localData);
+                return;
+            }
+
+            if (self.xhrRequest) {
+                self.xhrRequest.abort();  
+            }
+            
+            self.xhrRequest = $.get(self.currentOptions.source, { term : value, limit: self.currentOptions.limit }, function (data) {
+                self.processResponse(value, data);
+                self.currentOptions.requestCallback(data);
+            }, (self.currentOptions.returnJSON) ? 'json' : '');
+         },
+         
+         selectOption : function () {
+            this.execBlur = true;
+            var selected = this.resultsElement.find('.current'), 
+                selectedData = selected 
+                               ? $.data(selected[0], 'autocomplete') 
+                               : {};
+            this.searchElement.val(this.currentOptions.formatResult(selectedData)).blur();
+            this.currentOptions.onSelectionCallback(selectedData);
+            this.clearAutocomplete();
+         },
+         
+         optionNavigation : function (upArrow) {
+            upArrow = upArrow || false;
+            this.execBlur = false;
+          
+            if (upArrow) {
+                this.currentSelect = (currentSelect == 0) ? this.resultsElementChildren - 1 : --this.currentSelect;
+            } else {
+                this.currentSelect = (++this.currentSelect % this.resultsElementChildren)
+            }
+            
+            this.resultsElement.find('.current')
+                .removeClass('current')
+                .end()
+                .find('li:eq(' + this.currentSelect + ')')
+                .addClass('current')
+                .end();
+         },
+         
+         limitNumberOfItems : function (dataLength) {
+		    return this.currentOptions.limit && this.currentOptions.limit < dataLength
+			    ? this.currentOptions.limit
+			    : dataLength;
+	     },
+
+         getLocalData : function (term) {
+            var predicate = this.currentOptions.lookupPredicate; 
+
+            return $.grep(this.currentOptions.source, function (row) {
+                return predicate(row, term);
+            });
+         },
+
+         processResponse : function (term, data) {
+            // Cache results if cache is not disabled:
+            if (!this.currentOptions.noCache) {
+                this.responseCache[term] = data;
+            }
+
+            this.fillSuggestionsList(term, data);   
+         },
+
+         fillSuggestionsList : function (term, data) {
+            if (data.length == 0) {
+                this.clearAutocomplete();
+                return;
+            }
+
+            var totalItems = this.limitNumberOfItems(data.length),
+                docFragment = document.createDocumentFragment(),
+                listItem, resultItem, formatedItem;
+
+            this.resultsElement.empty();
+            for (var i = 0; i < totalItems; ++i) {
+                formatedItem = this.currentOptions.formatItem(data[i]);
+                listItem = document.createElement('li');
+                listItem.innerHTML = Autocomplete.highlightTerm(formatedItem, term);
+                docFragment.appendChild(listItem);
+                $.data(listItem, 'autocomplete', data[i]);
+            }
+            this.resultsElement.append(docFragment).show();
+            this.resultsElementChildren = $('li', this.resultsElement).length;
+            this.currentSelect = -1;
+         },
+        
+         bindEvents : function () {
+            var self = this;
+
+            self.searchElement.keydown(function (ev) {
+                var keyCode = ev.keyCode || window.event.keyCode, 
+                    keyCodes = Autocomplete.keyCodes;
+              
+                switch (keyCode) {
+                    case keyCodes.ENTER: //Enter
+                        self.selectOption();
+                        break;
+                    case keyCodes.ESCAPE: //Escape
+                        self.clearAutocomplete(); 
+                        break;
+                    case keyCodes.UP: //Up and down arrows
+                    case keyCodes.DOWN:
+                        self.optionNavigation.call(self, keyCode == keyCodes.UP ? true : false);
+                        self.currentOptions.upDownArrowsCallback.call(self, keyCode);
+                        break;
+                }
+            }).keyup(function (ev) {
+                var that = $(this),
+                    keyCode = ev.keyCode || window.event.keyCode, 
+                    keyCodes = Autocomplete.keyCodes;
+                    
+                if ((keyCode >= keyCodes.LETTERS_NUMBERS_MIN && keyCode <= keyCodes.LETTERS_NUMBERS_MAX) 
+                    || (keyCode >= keyCodes.NUM_PAD_MIN && keyCode <= keyCodes.NUM_PAD_MAX) 
+                    || keyCode == keyCodes.SUBTRACT 
+                    || keyCode == keyCodes.FORWARD_SLASH || keyCode == keyCodes.BACK_SLASH 
+                    || keyCode == keyCodes.BACKSPACE 
+                    || keyCode == keyCodes.DELETE) { //Only numbers, letters, hyphen, back slash, forward slash, backspace, delete
+                    if (self.currentOptions.deferRequestBy > 0) {
+                        self.timeOut = setTimeout(function () {
+                            self.autocomplete(that.val());
+                        }, self.currentOptions.deferRequestBy);
+                    } else {
+                        self.autocomplete(that.val());
+                    }
+                }   
+            }).focus(function () {
+                self.clearAutocomplete();
+                self.currentOptions.onFocusCallback.call(self, $(this));
+            }).blur(function () {
+                if (self.execBlur) {
+                    self.clearAutocomplete();
+                    self.currentOptions.onBlurCallback.call(self, $(this));
+                }
+            });
+            
+            self.resultsElement.mouseenter(function () {
+                self.execBlur = false;
+            }).mouseleave(function () {
+                self.execBlur = true;
+            }).mouseover(function (ev) {
+                if (ev.target.nodeName && ev.target.nodeName.toUpperCase() == 'LI') {
+                    self.resultsElement.find('.current').removeClass('current');
+                    $(ev.target).addClass('current');
+                }
+            }).click(function () {
+                self.selectOption();
+            });
+         }
+    
     };
     
     $.fn.autocomplete = function (options) {   
